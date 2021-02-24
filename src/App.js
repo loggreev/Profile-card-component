ae70b6fcd01a0bc6d914b02ad3f92d3990538f43
loggreev <70668891+loggreev@users.noreply.github.com>
2021-02-23 19:24:46 -0500

    add card addition and removal

--- src/App.js
+++ src/App.js
@@ -1,29 +1,81 @@
+import React from 'react';
 import './App.css';
 import ProfileCard from './components/ProfileCard';
 import avatar1 from "./images/image-victor.jpg";
 import avatar2 from "./images/australian-victor.jpg";
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super()
+    this.addCard = this.addCard.bind(this);
+    this.removeCard = this.removeCard.bind(this);
+    this.state = {
+      numCards: 2,
+      cards: [
+        <ProfileCard
+          key="initialcard1"
+          avatar={avatar1}
+          personalInfo={{ name: "Victor Crest", age: 26, location: "London" }}
+          stats={[
+            { number: "80k", tag: "Followers" },
+            { number: "803k", tag: "Likes" },
+            { number: "1.4k", tag: "Photos" }]}
+        />,
+
+        <ProfileCard
+          key="initialcard2"
+          avatar={avatar2}
+          personalInfo={{ name: "Australian Victor Crest", age: 62, location: "Australia" }}
+          stats={[
+            { number: "100M", tag: "Followers" },
+            { number: "1.0B", tag: "Likes" },
+            { number: "1", tag: "Photos" }]}
+        />
+      ]
+    };
+  }
+
+  addCard() {
+    let id = this.state.numCards + 1;
+
+    return this.setState({
+      numCards: id,
+      cards: [...this.state.cards,
+      < ProfileCard
+        key={id.toString()}
+        personalInfo={{ name: "John Doe", age: id, location: "Nowhere" }}
+        stats={
+          [
+            { number: id, tag: "Followers" },
+            { number: id, tag: "Likes" },
+            { number: id, tag: "Photos" }]}
+      />
+      ]
+    });
+  }
+
+  removeCard() {
+    let id = this.state.numCards - 1;
+    id = id < 0 ? 0 : id; //there cannot be negative numbers of cards
+
+    return this.setState({
+      numCards: id,
+      cards: [...this.state.cards].slice(0, id)
+    });
+  }
+
+  render = () =>
+  (
     <div className="App">
-      <ProfileCard
-        avatar={avatar1}
-        personalInfo={{ name: "Victor Crest", age: 26, location: "London" }}
-        stats={[
-          { number: "80k", tag: "Followers" },
-          { number: "803k", tag: "Likes" },
-          { number: "1.4k", tag: "Photos" }]}
-      />
-      <ProfileCard
-        avatar={avatar2}
-        personalInfo={{ name: "Australian Victor Crest", age: 62, location: "Australia" }}
-        stats={[
-          { number: "100M", tag: "Followers" },
-          { number: "1.0B", tag: "Likes" },
-          { number: "1", tag: "Photos" }]}
-      />
+      <div className="cardInteractionButtons">
+        <button onClick={this.addCard} id="addButton">+</button>
+        <button onClick={this.removeCard} id="removeButton">-</button>
+      </div>
+      {this.state.cards}
     </div>
   );
+
 }
 
 export default App;
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,11 @@
 function Avatar(props) {
+    let avatar = props.avatar;
+    if (!avatar) {
+        avatar = "https://via.placeholder.com/96";
+    }
+
     return (
-        <img src={props.avatar} className="Avatar" alt="Avatar"></img>
+        <img src={avatar} className="Avatar" alt="Avatar"></img>
     )
 }
 
